a86812c4f3b7985dd4c22fa8cc279d51eb7efdac
JahazielHernandezHoyos <jahazielhernandezhoyoz@gmail.com>
2023-05-01 14:40:29 -0500

    Correction styles and delete bugs

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ function createWindow () {
     },
   });
 
+  win.maximize(); // Para maximizar la ventana
   win.loadFile('frontend/index.html');
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools(); // Para abrir la consola de desarrollador
 
   // Run the python script
   const python = spawn('python', [path.join(__dirname, 'backend', 'main.py')]);
